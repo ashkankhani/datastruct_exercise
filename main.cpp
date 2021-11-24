edab83fc9a36b304a5814798eeac8420d5510a6f
Ashkan Khani <drashkankhani@gmail.com>
2021-11-24 08:46:00 +0330

    write switch and menu handling section

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-    int gozine;
+    int gozine,n;
 
     cout << "gozine morede nazar ra vared konid:\n";
     cout << "1.daryafte adad az voroodi\n";
@@ -21,7 +21,37 @@ int main(){
 
     while(gozine){
 
-        //switch and ....
+        switch (gozine)
+        {
+        case 1:
+            //array input
+            break;
+        case 2:
+            //darj
+            break;
+        case 3:
+            //hazf
+            break;
+        case 4:
+            //sorting
+            break;
+        case 5:
+            //display array
+            break;
+        case 6:
+            //save array
+            break;
+        case 7:
+            //restore array
+            break;
+        case 0:
+            //exit
+            break;
+        
+        default:
+            //wrong choice
+            break;
+        }
 
 
 
