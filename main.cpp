c79b27295ec21c03d79a4cde046fcee59aee925c
Ashkan Khani <drashkankhani@gmail.com>
2021-11-24 08:35:11 +0330

    create a base menu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,42 @@ using namespace std;
 
 int main(){
 
+    int gozine;
+
+    cout << "gozine morede nazar ra vared konid:\n";
+    cout << "1.daryafte adad az voroodi\n";
+    cout << "2.darj\n";
+    cout << "3.hazf\n";
+    cout << "4.sort kardan arraye\n";
+    cout << "5.namayeshe arraye\n";
+    cout << "6.save arraye\n";
+    cout << "7.restore arraye\n";
+    cout << "0.exit\n";
+
+    cin >> gozine;
+
+    while(gozine){
+
+        //switch and ....
+
+
+
+
+
+
+        cout << "gozine morede nazar ra vared konid:\n";
+        cin >> gozine;
+    }
+
+
+    cout << "shoma az menu kharej shodid";
+    cin.get();
+    cin.get();
+
 
     
+
+
+
+
 }
