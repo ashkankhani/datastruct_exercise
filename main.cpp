1bc111dda8e395836023f216e93da91f9e67a228
Ashkan Khani <drashkankhani@gmail.com>
2021-12-03 14:12:44 +0330

    finished! :)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
 using namespace std;
 
 
@@ -60,26 +61,89 @@ int main(){
                     new_array[i] = array[i];
                 }
             }
+            n++;
             delete[] array;
             array = new_array;
-            for(int i = 0;i<n+1;i++){
+            cout << "new array is:\n";
+            for(int i = 0;i<n;i++){
                 cout << array[i] << " ";
             }
+            cout << "\nnew array is ready for use ;)\n";
 
         }else if(gozine == 3){
+            int index , * new_array;
+            cout << "enter a index witch you need to be deleted:\n";
+            cin >> index;
+            new_array = new int[n - 1];
+            for(int i = 0;i<n;i++){
+                if(i<index){
+                    new_array[i] = array[i];
+                }else{
+                    new_array[i] = array[i + 1];
+                }
+            }
+            n--;
+            delete[] array;
+            array = new_array;
+            cout << "new array is:\n";
+            for(int i = 0;i<n;i++){
+                cout << array[i] << " ";
+            }
+            cout << "\narray is ready for use ;)\n";
+
 
         }else if(gozine == 4){
+            int temp;
+            for(int i = 0;i<n-1;i++){
+                for(int j = i + 1;j<n;j++){
+                    if(array[j] < array[i]){
+                        temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                        
+                    }
+                }
+            }
+            cout << "sorted array = \n";
+            for(int i = 0;i<n;i++){
+                cout << array[i] << " ";
+            }
+            cout << "\narray is ready for use ;)\n";
+
+        
             
         }else if(gozine == 5){
+            cout << "your array is:\n";
+            for(int i = 0;i<n;i++){
+                cout << array[i] << " ";
+            }
+            cout << endl;
             
         }else if(gozine == 6){
-            
+            ofstream myfile;
+            myfile.open("array.txt",ios::out);
+            if(myfile.is_open()){
+                myfile << n << endl;
+                for(int i = 0;i<n;i++){
+                    myfile << array[i] << endl;
+                }
+                myfile.close();
+            }
         }else if(gozine == 7){
+            ifstream myfile("array.txt");
+            string line;
+            myfile >> n;
+            int i = 0;
+            delete[] array;
+            array = new int[n];
+            while(getline(myfile,line)){
+                myfile >> array[i++];
+            }
             
         }else if(gozine == 0){
             break;
         }else{
-            break;
+            continue;
         }
 
         
