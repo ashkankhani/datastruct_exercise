bb2f5b36791e8a94a10f693bb4ab40ae2f936403
Ashkan Khani <drashkankhani@gmail.com>
2021-12-15 23:31:35 +0330

    first commit

---
+++ 14.cpp
@@ -0,0 +1,45 @@
+
+// A simple CPP program to introduce
+// a linked list
+#include <iostream>
+using namespace std;
+ 
+struct Student {
+    string first_name;
+    string lastname;
+    unsigned long int std_num;
+    float moadel;
+};
+ 
+
+int main()
+{
+    Student *start;
+    int gozine;
+    start = new Student();
+
+    cout << "be menue barname khosh amadid!";
+    cout << "1.afzoodane daneshjoo be list\n";
+    cout << "2.hazfe daneshjoo az list\n";
+    cout << "3.namayeshe liste daneshjooyan\n";
+    cout << "4.search by name and lastname\n";
+    cout << "5.moratab sazi\n";
+    cout << "6.binary search\n";
+    cout << "0.exit\n";
+
+    cin >> gozine;
+
+    
+
+
+    
+
+
+
+
+ 
+  
+ 
+    return 0;
+}
+ 
