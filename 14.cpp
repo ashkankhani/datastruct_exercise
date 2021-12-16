840b0c104606e52b114004b095af8860afb38fd9
Ashkan Khani <drashkankhani@gmail.com>
2021-12-16 18:34:10 +0330

    write some codes

--- 14.cpp
+++ 14.cpp
@@ -1,24 +1,78 @@
-
-// A simple CPP program to introduce
-// a linked list
 #include <iostream>
 using namespace std;
  
 struct Student {
-    string first_name;
-    string lastname;
+    string first_name,lastname;
     unsigned long int std_num;
     float moadel;
-};
- 
+    Student *link;
+}*start;
+
+
+void insert_std(Student **start,
+int index,
+string first_name ,
+string last_name , 
+unsigned long int std_num ,
+float moadel){
+    Student *n;
+    n = new Student();
+    n->first_name = first_name;
+    n->lastname = last_name;
+    n->std_num = std_num;
+    n -> moadel = moadel;
+    
+    if(index){
+        //
+        Student *p;
+        p = *start;
+        //darj dar vasat
+        int i = 0;
+        while(p!=NULL && i < index - 1){
+            p = p->link;
+            i++;
+        }
+        if(p!=NULL){
+            n->link = p->link;
+            p->link = n;
+        }else{
+            cout << "index is out of range!";
+        }
+        
+
+
+    }else{
+        n->link = NULL;
+        *start = n;
+    }
+
+    
+
+
+
+}
+
+void display_students(Student *start){
+    Student *p;
+    p = start;
+    cout << "first_name" <<"\t|\t"<< "lastname" << "\t|\t" << "std_number" << "\t|\t"<< "moadel" <<endl;
+    while(p!=NULL){
+        cout << p->first_name <<"\t|\t"<<p -> lastname << "\t|\t" << p->std_num << "\t|\t" << p -> moadel <<endl;
+        p = p->link;
+    }
+
+}
 
 int main()
 {
-    Student *start;
-    int gozine;
+    
+    int gozine,index;
+    string first_name,lastname;
+    unsigned long int std_num;
+    float moadel;
     start = new Student();
 
-    cout << "be menue barname khosh amadid!";
+    cout << "be menue barname khosh amadid!\n";
     cout << "1.afzoodane daneshjoo be list\n";
     cout << "2.hazfe daneshjoo az list\n";
     cout << "3.namayeshe liste daneshjooyan\n";
@@ -26,10 +80,43 @@ int main()
     cout << "5.moratab sazi\n";
     cout << "6.binary search\n";
     cout << "0.exit\n";
-
+    cout << "lotfan yek gozine entekhab konid:\n";
     cin >> gozine;
 
-    
+    while(gozine){
+        if(gozine == 1){
+            cout << "enter firstname:\n";
+            cin >> first_name;
+            cout << "enter lastname:\n";
+            cin >> lastname;
+            cout << "enter stdnum:\n";
+            cin >> std_num;
+            cout << "enter moadel:\n";
+            cin >> moadel;
+            cout << "enter the index of insertion:\n";
+            cin >> index;
+            insert_std(&start ,index, first_name , lastname , std_num , moadel);
+            display_students(start);
+
+
+        }else if(gozine == 2){
+
+        }else if(gozine == 3){
+            
+        }else if(gozine == 4){
+            
+        }else if(gozine == 5){
+            
+        }else if(gozine == 6){
+            
+        }
+
+
+
+    cout << "lotfan yek gozine entekhab konid:\n";
+    cin >> gozine;
+
+    }
 
 
     
